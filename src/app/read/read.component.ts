45f512f3c545c69fddf80ed6c7874af4088349c5
evagolabek <golabek.eva@gmail.com>
2018-12-02 20:25:37 +0100

    read component html

--- src/app/read/read.component.ts
+++ src/app/read/read.component.ts
@@ -15,7 +15,6 @@ export class ReadComponent implements OnInit {
   things: Observable<Thing[]>;
 
 //access the store, where you pass appState 
-//next you access ngrx store
 //'thing' comes from AppModule where you bind it to the reducer
   constructor(private store: Store<AppState>) {
     this.things = store.select('thing');
