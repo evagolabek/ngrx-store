66f5f591d6dda8aba344c06411011099921c357a
evagolabek <golabek.eva@gmail.com>
2018-12-02 20:36:58 +0100

    create component store access and add method

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Thing } from './../models/thing.model';
+import { AppState } from './../app.state';
+
+//you dispatching an action here so you need actions but you don't need an Observable
+import * as ThingActions from './../actions/thing.actions';
 
 @Component({
   selector: 'app-create',
@@ -7,7 +13,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  //as previously you want to access the store
+  constructor(private store: Store<AppState>) { }
+
+  //you want to call a method 
+  addThing(name, url) {
+    this.store.dispatch(new ThingActions.AddThing({name: name , url: url}))
+  }
 
   ngOnInit() {
   }
